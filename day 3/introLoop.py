040a5fe70a6587be7432bf47e96fe764a5028f84
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-03-26 19:24:45 +0530

    Add files via upload

---
+++ day 3/introLoop.py
@@ -0,0 +1,15 @@
+
+x = 0
+
+print("_"*15 +"while Loop"+ "_"*15)
+while True:
+    if x == 5:
+        break
+    print(x)
+    x += 1
+
+print("_"*15 +"for loop"+ "_"*15)
+
+for i in range(0,6):
+    print(i)
+
