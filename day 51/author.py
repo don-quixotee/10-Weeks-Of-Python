58040de3f7b3cba2d5c9b8ebc84e3f5231d68eb8
Abhishek singh <60599305+don-quixotee@users.noreply.github.com>
2020-05-01 23:14:26 +0530

    Add files via upload

---
+++ day 51/author.py
@@ -0,0 +1,30 @@
+class Author:
+
+    authorList = []
+    
+    def __init__(self, first_name, last_name, email,profession,password ):
+        self.firstName = first_name
+        self.lastName = last_name
+        self.email = email
+        self.password = password
+        self.profession = profession
+        Author.authorList.append(Self)
+
+
+    def display(self):
+        return Author.authorList
+
+
+
+auth1 = ["abhishek", "singh", "ab@gmail.com", "web programmer","123"]
+auth2 = ["abhishek", "singh", "ab@gmail.com", "web programmer","123"]
+auth3 = ["abhishek", "singh", "ab@gmail.com", "web programmer","123"]
+
+auth=[auth1,auth2,auth3]
+
+
+
+
+
+
+
